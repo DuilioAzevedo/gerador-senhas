7522f6b53f0101be483227d34ab68f2b5402400b
DuilioAzevedo <duilio_uei@hotmail.com>
2025-02-07 05:03:51 -0300

    finish project

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+function generatePassword(length = 12) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    document.getElementById("password").value = password;
+}
+
+document.getElementById("generateBtn").addEventListener("click", function() {
+    generatePassword();
+});
